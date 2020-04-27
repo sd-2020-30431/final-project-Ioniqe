288b8c692bb04ad904b6db33fd19d16a5edc9605
Ioniqe <bozdogioana@yahoo.com>
2020-04-27 17:31:56 +0300

    Emotion Detection based on Webcam Image

---
+++ Project/Project/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.capture_butt = new System.Windows.Forms.Button();
+            this.cameraResolution = new System.Windows.Forms.ComboBox();
+            this.detect_butt = new System.Windows.Forms.Button();
+            this.results = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(866, 536);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // capture_butt
+            // 
+            this.capture_butt.Location = new System.Drawing.Point(269, 639);
+            this.capture_butt.Name = "capture_butt";
+            this.capture_butt.Size = new System.Drawing.Size(173, 41);
+            this.capture_butt.TabIndex = 1;
+            this.capture_butt.Text = "Capture";
+            this.capture_butt.UseVisualStyleBackColor = true;
+            this.capture_butt.Click += new System.EventHandler(this.captureButt_Click);
+            // 
+            // cameraResolution
+            // 
+            this.cameraResolution.FormattingEnabled = true;
+            this.cameraResolution.Location = new System.Drawing.Point(245, 599);
+            this.cameraResolution.Name = "cameraResolution";
+            this.cameraResolution.Size = new System.Drawing.Size(435, 21);
+            this.cameraResolution.TabIndex = 3;
+            this.cameraResolution.SelectedIndexChanged += new System.EventHandler(this.cameraResolution_SelectedIndexChanged);
+            // 
+            // detect_butt
+            // 
+            this.detect_butt.Location = new System.Drawing.Point(485, 639);
+            this.detect_butt.Name = "detect_butt";
+            this.detect_butt.Size = new System.Drawing.Size(173, 41);
+            this.detect_butt.TabIndex = 4;
+            this.detect_butt.Text = "Detect Emotion";
+            this.detect_butt.UseVisualStyleBackColor = true;
+            this.detect_butt.Click += new System.EventHandler(this.detect_butt_Click);
+            // 
+            // results
+            // 
+            this.results.AutoSize = true;
+            this.results.Location = new System.Drawing.Point(957, 28);
+            this.results.Name = "results";
+            this.results.Size = new System.Drawing.Size(42, 13);
+            this.results.TabIndex = 5;
+            this.results.Text = "Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1270, 692);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.detect_butt);
+            this.Controls.Add(this.cameraResolution);
+            this.Controls.Add(this.capture_butt);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Camera";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button capture_butt;
+        private System.Windows.Forms.ComboBox cameraResolution;
+        private System.Windows.Forms.Button detect_butt;
+        private System.Windows.Forms.Label results;
+    }
+}
+
---
+++ Project/Project/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DarrenLee.Media;
+
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.CodeDom.Compiler;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+
+        //---------------
+        // Replace <Subscription Key> with your valid subscription key.
+        const string subscriptionKey = "c136b8ab43d740bd80735e3be98551a6";
+
+        // replace <myresourcename> with the string found in your endpoint URL
+        const string uriBase =
+            "https://eastus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+        //---------------
+
+
+        //int count = 1;
+        Camera myCamera = new Camera();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            GetInfo();
+            myCamera.OnFrameArrived += MyCamera_OnFrameArrived;
+        }
+
+        public void GetInfo()
+        {
+            var cameraResolutions = myCamera.GetSupportedResolutions();
+
+            foreach(var r in cameraResolutions)
+            {
+                cameraResolution.Items.Add(r);
+            }
+
+            cameraResolution.SelectedIndex = 7;
+        }
+
+        public void MyCamera_OnFrameArrived(object source, FrameArrivedEventArgs e)
+        {
+            Image img = e.GetFrame();
+            pictureBox1.Image = img;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myCamera.Stop();
+        }
+
+        private void cameraResolution_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myCamera.Start(cameraResolution.SelectedIndex);
+        }
+
+        private void captureButt_Click(object sender, EventArgs e)
+        {
+            //string filename = Application.StartupPath + @"\" + "Image" + count.ToString(); //E:\an3\sem2\SD\Images
+            //string filename = @"E:\an3\sem2\SD\Project\Project\bin\Debug\Images\Image" + count.ToString(); //--good one
+            string filename = @"E:\an3\sem2\SD\Project\Project\bin\Debug\Images\Image";
+            myCamera.Capture(filename);
+            //count++;
+
+            Process.Start(@"E:\an3\sem2\SD\Project\Project\bin\Debug\Images");
+
+        }
+
+        private void detect_butt_Click(object sender, EventArgs e)
+        {
+            //------AI------
+            launchEmotionDetection();
+        }
+
+        private void launchEmotionDetection()
+        {
+            string imageFilePath = @"E:\an3\sem2\SD\Project\Project\bin\Debug\Images\Image.jpg";
+
+            if (File.Exists(imageFilePath))
+            {
+                try
+                {
+                    MakeAnalysisRequest(imageFilePath);
+                    results.Text = "\nWait a moment for the results to appear.\n";
+                    //Console.WriteLine("\nWait a moment for the results to appear.\n");
+                }
+                catch (Exception e)
+                {
+                    results.Text = "\n" + e.Message + "\nPress Enter to exit...\n";
+                    //Console.WriteLine("\n" + e.Message + "\nPress Enter to exit...\n");
+                }
+            }
+            else
+            {
+                results.Text = "\nInvalid file path.\nPress Enter to exit...\n";
+                //Console.WriteLine("\nInvalid file path.\nPress Enter to exit...\n");
+            }
+            Console.ReadLine();
+        }
+
+        async void MakeAnalysisRequest(string imageFilePath)
+        {
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add(
+                "Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request parameters. A third optional parameter is "details".
+            //string requestParameters = "returnFaceId=true&returnFaceLandmarks=false" +
+            //    "&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses," +
+            //    "emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+
+            string requestParameters = "returnFaceId=true&returnFaceLandmarks=false" +
+                "&returnFaceAttributes=emotion";
+
+            // Assemble the URI for the REST API Call.
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            // Request body. Posts a locally stored JPEG image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json"
+                // and "multipart/form-data".
+                content.Headers.ContentType =
+                    new MediaTypeHeaderValue("application/octet-stream");
+
+                // Execute the REST API call.
+                response = await client.PostAsync(uri, content);
+
+                // Get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+
+                // Display the JSON response.
+
+                results.Text = "\nResponse:\n";
+                results.Text = JsonPrettyPrint(contentString);
+
+            }
+        }
+
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            int offset = 0;
+            int indentLength = 3;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\'':
+                        if (quote) ignore = !ignore;
+                        break;
+                }
+
+                if (quote)
+                    sb.Append(ch);
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case '}':
+                        case ']':
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', --offset * indentLength));
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+
+
+    }
+}
